c24d337cce5e7fc669f7834c71bef9a7ab9c7ad4
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:39:30 +0100

    Update create.php

--- service_center/create.php
+++ service_center/create.php
@@ -7,8 +7,8 @@
 <!DOCTYPE html>
 <html translate="no">
     <head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <meta name="author" content="Antonio Scardace">
         
@@ -18,7 +18,7 @@
         <link rel="stylesheet" href="src/css/main.css">
         <link rel="stylesheet" href="src/css/create.css">
 
-		<title>Create Operation</title>
+	<title>Create Operation</title>
     </head>
     <body>
 
