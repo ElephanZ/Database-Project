eeaa699136609939cf34ab0991cbc9ad8fa1294e
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:42:31 +0100

    Update Client.php

--- service_center/src/php/classes/Client.php
+++ service_center/src/php/classes/Client.php
@@ -9,25 +9,25 @@
 			$this->db = new Database;
 		}
 
-        private function checkIfExists($fc) {
-            $res = $this->db->getRows('client', 'fc', "fc='$fc'");
-            return $res && count($res) > 0;
-        }
+		private function checkIfExists($fc) {
+		    $res = $this->db->getRows('client', 'fc', "fc='$fc'");
+		    return $res && count($res) > 0;
+		}
 
-        public function insertIfNotExists($fc, $name, $sname, $dbirth) {
-            if ($this->checkIfExists($fc)) return;
-            $this->db->insert('client', 'fc, name, surname, date_birth', "'$fc', '$name', '$sname', '$dbirth'");
-        }
+		public function insertIfNotExists($fc, $name, $sname, $dbirth) {
+		    if ($this->checkIfExists($fc)) return;
+		    $this->db->insert('client', 'fc, name, surname, date_birth', "'$fc', '$name', '$sname', '$dbirth'");
+		}
 
-        private function checkIfOwnContact($value, $fc) {
-            $res = $this->db->getRows('client c JOIN own o ON c.fc = o.client_id', 'contact_id', "contact_id='$value' AND client_id='$fc'");
-            return $res && count($res) == 1;
-        }
+		private function checkIfOwnContact($value, $fc) {
+		    $res = $this->db->getRows('client c JOIN own o ON c.fc = o.client_id', 'contact_id', "contact_id='$value' AND client_id='$fc'");
+		    return $res && count($res) == 1;
+		}
 
-        public function checkAndOwn($fc, $value) {
-            if ($this->checkIfOwnContact($value, $fc)) return;
-            $this->db->insert('own', 'contact_id, client_id', "'$value', '$fc'");
-        }
+		public function checkAndOwn($fc, $value) {
+		    if ($this->checkIfOwnContact($value, $fc)) return;
+		    $this->db->insert('own', 'contact_id, client_id', "'$value', '$fc'");
+		}
 		
 	}
 
