b22c46e5ff5c636210b00494fcb2144b01aaac9c
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:45:42 +0100

    Update Contact.php

--- service_center/src/php/classes/Contact.php
+++ service_center/src/php/classes/Contact.php
@@ -6,18 +6,18 @@
 		private $db;
 
 		public function __construct() {
-			$this->db = new Database;
+		    $this->db = new Database;
 		}
 
 		private function checkIfExists($value) {
-            $res = $this->db->getRows('contact', 'value', "value='$value'");
-            return $res && count($res) > 0;
-        }
+		    $res = $this->db->getRows('contact', 'value', "value='$value'");
+		    return $res && count($res) > 0;
+		}
 
-        public function insertIfNotExists($kind, $value) {
-            if ($this->checkIfExists($value)) return;
-            $this->db->insert('contact', 'kind, value', "'$kind', '$value'");
-        }
+		public function insertIfNotExists($kind, $value) {
+		    if ($this->checkIfExists($value)) return;
+		    $this->db->insert('contact', 'kind, value', "'$kind', '$value'");
+		}
 		
 	}
 
