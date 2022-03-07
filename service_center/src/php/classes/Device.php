d21cd7ecb94255ffc4861997f050b279d6cf60c1
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:43:06 +0100

    Update Device.php

--- service_center/src/php/classes/Device.php
+++ service_center/src/php/classes/Device.php
@@ -6,18 +6,18 @@
 		private $db;
 
 		public function __construct() {
-			$this->db = new Database;
+		    $this->db = new Database;
 		}
 
 		private function checkIfExists($sn) {
-            $res = $this->db->getRows('device', 'sn', "sn='$sn'");
-            return $res && count($res) > 0;
-        }
+		    $res = $this->db->getRows('device', 'sn', "sn='$sn'");
+		    return $res && count($res) > 0;
+		}
 
-        public function insertIfNotExists($sn, $brand, $model, $kind, $fc) {
-            if ($this->checkIfExists($sn)) return;
-            $this->db->insert('device', 'sn, brand, model, kind, client_id', "'$sn', '$brand', '$model', '$kind', '$fc'");
-        }
+		public function insertIfNotExists($sn, $brand, $model, $kind, $fc) {
+		    if ($this->checkIfExists($sn)) return;
+		    $this->db->insert('device', 'sn, brand, model, kind, client_id', "'$sn', '$brand', '$model', '$kind', '$fc'");
+		}
 		
 	}
 
