6ba01d24f73892740f4b6e5826ce003e6dca81aa
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:39:07 +0100

    Update index.php

--- service_center/index.php
+++ service_center/index.php
@@ -7,8 +7,8 @@
 <!DOCTYPE html>
 <html translate="no">
     <head>
-		<meta charset="utf-8"/>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta charset="utf-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <meta name="author" content="Antonio Scardace">
         
@@ -18,7 +18,7 @@
         <link rel="stylesheet" href="src/css/main.css">
         <link rel="stylesheet" href="src/css/home.css">
 
-		<title>Home</title>
+	<title>Home</title>
     </head>
     <body>
 
