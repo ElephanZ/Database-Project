073172bd5f95171169e693fd20568c359e8a7f94
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-03-07 20:42:19 +0100

    Update Accessory.php

--- service_center/src/php/classes/Accessory.php
+++ service_center/src/php/classes/Accessory.php
@@ -6,16 +6,16 @@
 		private $db;
 
 		public function __construct() {
-			$this->db = new Database;
+		    $this->db = new Database;
 		}
 
-        public function insert($name, $op_id, $dev_id, $descr) {
-            $this->db->insert(
-				'accessory',
-                'name, operation_id, device_id, note',
-                "'$name', $op_id, '$dev_id', '$descr'"
-            );
-        }
+		public function insert($name, $op_id, $dev_id, $descr) {
+		    $this->db->insert(
+					'accessory',
+			'name, operation_id, device_id, note',
+			"'$name', $op_id, '$dev_id', '$descr'"
+		    );
+		}
 		
 	}
 
