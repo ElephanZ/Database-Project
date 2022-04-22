d1dddb1713e088a5eb69dabbfecaa82684b12df0
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-04-22 14:29:25 +0200

    Update functions.sql

--- sql/functions.sql
+++ sql/functions.sql
@@ -1,6 +1,7 @@
 DELIMITER $$  
 
 DROP FUNCTION IF EXISTS calculateVAT $$
+
 CREATE FUNCTION calculateVAT(cost DECIMAL(5, 2), labor INT(3), vat INT(2)) RETURNS DECIMAL(6, 2)
 BEGIN
     DECLARE amount DECIMAL(6, 2) DEFAULT (labor + cost);
